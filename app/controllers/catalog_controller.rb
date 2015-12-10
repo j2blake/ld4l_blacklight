570c73952cdce058e1fadb2dd89de30dcb882320
Muhammad Javed <mj495@cornell.edu>
2015-12-10 13:57:30 -0500

    some revert things from schema.xml

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -81,6 +81,7 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
+    config.add_index_field 'title_t', :label => 'Title'
     config.add_index_field 'title_display', :label => 'Title'
     config.add_index_field 'title_vern_display', :label => 'Title'
     config.add_index_field 'author_display', :label => 'Author'
@@ -95,6 +96,7 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display 
+    config.add_show_field 'title_t', :label => 'Title'
     config.add_show_field 'title_display', :label => 'Title'
     config.add_show_field 'title_vern_display', :label => 'Title'
     config.add_show_field 'subtitle_display', :label => 'Subtitle'
